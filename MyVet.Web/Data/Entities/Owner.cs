a72acf7c9d03e06ae76b425aa55542f6c524102e
daniel martinez <darriel-martinez@live.com>
2020-04-06 14:03:25 -0500

    DataBase And Object Clases

---
+++ MyVet.Web/Data/Entities/Owner.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyVet.Web.Data.Entities
+{
+    public class Owner
+    {
+        
+        public int Id { get; set; }
+
+        [Display(Name = "Document")]
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Document { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Display(Name ="First Name")]
+        public String FirstName { get; set; }
+
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [Display(Name = "Last Name")]
+        public String LastName { get; set; }
+
+
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [Display(Name = "Fixed Phone")]
+        public String  FixedPhone { get; set; }
+
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [Display(Name = "Cell Phone")]
+        public String CellPhone { get; set; }
+
+        [MaxLength(100, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        public String Address { get; set; }
+
+
+        [Display(Name = "Full Name")]
+        public String FullName => $"{FirstName} {LastName}";
+
+
+
+        [Display(Name = "Owner")]
+        public String FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
+
+        public ICollection<Pet> Pets { get; set; }
+
+        public ICollection<Agenda> Agendas { get; set; }
+
+
+    }
+}
