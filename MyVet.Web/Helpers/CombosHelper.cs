e5585264f800e5dec347f061b3dcb4b6b232e081
daniel martinez <darriel-martinez@live.com>
2020-04-08 16:17:19 -0500

    Add Pet and Show

---
+++ MyVet.Web/Helpers/CombosHelper.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using MyVet.Web.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyVet.Web.Helpers
+{
+    public class CombosHelper : ICombosHelper
+    {
+        private readonly DataContext _datacontext;
+
+        public CombosHelper(DataContext datacontext)
+        {
+            _datacontext = datacontext;
+        }
+
+        public IEnumerable<SelectListItem> GetComboPetTypes()
+        {
+            var list = _datacontext.PetTypes.Select(pt => new SelectListItem
+            {
+                Text = pt.Name,
+                Value = $"{pt.Id}"
+            }
+
+                )
+                .OrderBy(pt => pt.Text)
+                .ToList();
+
+            list.Insert(0, new SelectListItem
+            {
+                Text = "[Select a Pet Type]",
+                Value = "0"
+
+            });
+            return list;
+        }
+
+    }
+}
